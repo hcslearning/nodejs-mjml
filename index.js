f2466c6980a294c98f1e1baf2ad3199a9d329a1f
HCS Learning <learning@hardcybersoft.cl>
2021-01-24 04:56:53 -0300

    integracion con sendgrid

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const mjml2html = require('mjml');
 
 // para el server
 const http = require('http');
+const url = require('url');
 const server = new http.Server();
+
+function enviarMail(destinatario, subject, plainMail, htmlMail) {
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey( process.env.API_KEY_SENDGRID );
+    const msg = {
+        to: destinatario,
+        from: process.env.MAIL_FROM,
+        subject: subject,
+        text: plainMail,
+        html: htmlMail
+    };
+    sgMail.send( msg ).then( () => console.log("Mail enviado") ).catch( (err) => {console.log("Error al enviar el correo: ");console.dir(err)} );
+}
 
 
 server.on( 'request', (req, res) => {
@@ -18,6 +32,15 @@ server.on( 'request', (req, res) => {
 
     res.writeHead(200, {'Content-Type': 'text/html'});
     //res.write( "API KEY: "+process.env.API_KEY_SENDGRID+"<br /><br />" );
+    
+    // procesa query parameters from URL
+    const query = url.parse( req.url, true).query;
+    const destinatario = query.to;
+    
+	// enviar correo
+	console.dir( {destinatario, subject: "Correo de prueba", plain: "texto plano de prueba"} );
+	enviarMail( destinatario, "Correo de prueba", "texto plano de prueba", htmlObj.html);
+    
     res.write( htmlObj.html );
     res.end();
 });
