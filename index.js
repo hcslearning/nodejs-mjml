8d26a96c2556b3e6621fe1a6dd4c896a9e587bf9
HCS Learning <learning@hardcybersoft.cl>
2021-01-24 04:28:53 -0300

    add dotenv para API KEY

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
+// para cargar variables de .env file
+require('dotenv').config();
+
+// engine para plantillas de emails
 const mjml2html = require('mjml');
 
+// para el server
 const http = require('http');
 const server = new http.Server();
 
 
 server.on( 'request', (req, res) => {
-    const mjmlCode = `
-      <mjml>
-        <mj-body>
-          <mj-section>
-            <mj-column>
-              <mj-text>
-                Hello World!
-              </mj-text>
-            </mj-column>
-          </mj-section>
-        </mj-body>
-      </mjml>
-    `;
-    const mjmlCode2 = require('fs').readFileSync("template-ejemplo.mjml", "utf-8");
-
+	// carga contenido de archivo con plantilla en lenguaje MJML
+    const mjmlCode = require('fs').readFileSync("template-ejemplo.mjml", "utf-8");
     const options = {};
-    const htmlObj = mjml2html(mjmlCode2, options);
+    // convierte MJML a HTML apto para enviar correo HTML
+    const htmlObj = mjml2html(mjmlCode, options);
 
     res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.write( "API KEY: "+process.env.API_KEY_SENDGRID+"<br /><br />" );
     res.write( htmlObj.html );
     res.end();
 });
 
-server.listen(5000);
-console.log("Servidor escuchando en puerto 5000");
+// corre el servidor en el puerto especificado
+const port = 5500
+server.listen( port );
+console.log("Servidor escuchando en puerto "+port);
